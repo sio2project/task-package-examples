feb927e0e8c4a15b5395880b2d7e02cad6d43edf
niedziol <michalekniedziolka@gmail.com>
2020-04-23 15:51:18 +0200

    (tutorial_src) Upload sources to tutorial/tre.

---
+++ tutorial_src/tre/prog/tre2.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+if n == 0:
+    print("Too small to exist")
+    exit()
+
+for x in range(2):
+    for i in range(n + x):
+        print((n - i) * '.', end='')
+        print((2 * (i + 1) - 1) * '#', end='')
+        print((n - i) * '.')
+
+for i in range(2):
+    print(n * '.', end='')
+    print('#', end='')
+    print(n * '.')
---
+++ tutorial_src/tre/prog/treb1.py
@@ -0,0 +1,3 @@
+n = int(input())
+
+print("Too small to exist")
