65b0575953b887da9614fdc97d4195fc90a36ab0
Stanislaw Strzelecki <stasiek.strzelecki@gmail.com>
2020-04-28 00:59:09 +0200

    (tutorial src) Added gue package

---
+++ tutorial_src/gue/prog/gue.cpp
@@ -0,0 +1,16 @@
+#include "gue.h"
+
+int main()
+{
+    int n = init();
+    int st = 1, en = n, mid = (st + en) / 2;
+    while (st < en)
+    {
+        if (isGreater(mid))
+            st = mid + 1;
+        else
+            en = mid;
+        mid = (st + en) / 2;
+    }
+    answer(mid);
+}
