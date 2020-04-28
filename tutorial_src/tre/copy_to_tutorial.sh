ba5a4e8a4ff08648e2c792a17c1820b8bf2ca720
niedziol <michalekniedziolka@gmail.com>
2020-04-28 17:18:31 +0200

    (tutorial_src) Modify tre package, remove symlinks, change .tex, upload to tutorial.

--- tutorial_src/tre/copy_to_tutorial.sh
+++ tutorial_src/tre/copy_to_tutorial.sh
@@ -7,6 +7,11 @@ set -e
 _files=( doc/trezad.pdf prog/*.cpp prog/*.py in/* out/tre1ocen.out )
 
 _id=${PWD##*/}
+
+if [ ! -d "../../tutorial/$_id" ]; then
+    mkdir -v ../../tutorial/$_id
+fi
+
 for _file in "${_files[@]}"; do
     cp -v $_file ../../tutorial/$_id/$_file
 done
