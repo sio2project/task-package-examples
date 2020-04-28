1a3a502541f69e143dfdf8773c27eea59200baca
Stanislaw Strzelecki <stasiek.strzelecki@gmail.com>
2020-04-28 20:17:18 +0200

    Added public folder

---
+++ tutorial_src/gue/public/gue.cpp
@@ -0,0 +1,13 @@
+#include "gue.h"
+
+int main()
+{
+    int n = init();
+    if (greater(4)) {
+        answer(n);
+    } else if (greater(3) {
+        answer(4);
+    } else {
+        answer(1);
+    }
+}
---
+++ tutorial_src/gue/public/gue.h
@@ -0,0 +1,8 @@
+#ifndef _gue_h
+#define _gue_h
+
+int init();
+bool isGreater(int x);
+void answer(int x);
+
+#endif
